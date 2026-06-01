1fc560837263afde51656aa152552f57cb6d2096
Laren <Laren@gmail.com>
2026-06-01 12:00:05 +0530

    Add implementation of twoSum function to find indices of two numbers that add up to a target

---
+++ leetcode-167.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size();
+        int r=n-1;
+        int l=0;
+
+        while(l<r){
+            if((numbers[l]+numbers[r])>target){
+                r--;
+            }else if((numbers[l]+numbers[r])<target){
+                l++;
+            }else if( (numbers[l]+numbers[r])==target){
+                return {l+1,r+1};
+            }
+        }
+   return {-1,-1};
+    }
+};
