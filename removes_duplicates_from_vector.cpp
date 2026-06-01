343011b9f952e8f5b9e9d453293392fd2acb1d57
Laren <Laren@gmail.com>
2026-06-01 16:06:02 +0530

    Refactor duplicate removal logic to sort vector in descending order before erasing duplicates

--- removes_duplicates_from_vector.cpp
+++ removes_duplicates_from_vector.cpp
@@ -7,10 +7,13 @@ dulplicates in place such that each element appears only once and return the new
 #include<algorithm>
 using namespace std;
 int main(){
-    vector<int>v={10,9,8,7,5,5,4,4,2,1,0};
-  v.erase(unique(v.begin(),v.end()),v.end());
+    vector<int>v={1,2,3,4,5,9,10,10,10};
+   sort(v.begin(),v.end(),greater<int>());
+    v.erase(unique(v.begin(),v.end()),v.end());
 
    for(int i=0;i<v.size();i++){
     cout<<v[i]<<" ";
    }
+   cout<<endl;
+   cout<<v.size();
 }
