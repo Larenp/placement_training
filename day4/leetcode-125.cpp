9b813dbd826019f22ef2085af20e0be0d92f45e3
Laren <Laren@gmail.com>
2026-06-02 15:32:10 +0530

    Remove commented-out code for palindrome validation in leetcode-125.cpp

--- day4/leetcode-125.cpp
+++ day4/leetcode-125.cpp
@@ -5,35 +5,6 @@
 #include<cctype>
 using namespace std;
 
-
-bool valid_palindrom(string s1)
-{
-
-    string s;
-    for (int i=0;i<s1.length();i++)
-    {
-        if (isalnum(s1[i]))
-        {
-            s += tolower(s1[i]);
-        }
-    }
-    int l = 0;
-    int r = s.length() - 1;
-    while (l < r)
-    {
-        if (s[l] != s[r])
-        {
-            return false;
-        }
-        else
-        {
-            l++;
-            r--;
-        }
-    }
-    return true;
-}
-/*
 bool valid_palindrom(string s1)
 {
 
@@ -61,7 +32,7 @@ bool valid_palindrom(string s1)
     }
     return true;
 }
-*/
+
 int main()
 {
     string s1;
