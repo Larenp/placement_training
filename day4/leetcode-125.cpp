4011217a51fae3c63c978ccde1fedbc9d8fc26fb
Laren <Laren@gmail.com>
2026-06-02 15:28:40 +0530

    Add implementation for palindrome validation in leetcode-125.cpp
    - Created a function `valid_palindrom` to check if a given string is a palindrome, ignoring non-alphanumeric characters and case.
    - Included input handling in the `main` function to allow user interaction.
    - Added commented-out alternative implementations for future reference.

---
+++ day4/leetcode-125.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include<cctype>
+using namespace std;
+
+
+bool valid_palindrom(string s1)
+{
+
+    string s;
+    for (int i=0;i<s1.length();i++)
+    {
+        if (isalnum(s1[i]))
+        {
+            s += tolower(s1[i]);
+        }
+    }
+    int l = 0;
+    int r = s.length() - 1;
+    while (l < r)
+    {
+        if (s[l] != s[r])
+        {
+            return false;
+        }
+        else
+        {
+            l++;
+            r--;
+        }
+    }
+    return true;
+}
+/*
+bool valid_palindrom(string s1)
+{
+
+    string s;
+    for (auto c : s1)
+    {
+        if (isalnum(c))
+        {
+            s += tolower(c);
+        }
+    }
+    int l = 0;
+    int r = s.length() - 1;
+    while (l < r)
+    {
+        if (s[l] != s[r])
+        {
+            return false;
+        }
+        else
+        {
+            l++;
+            r--;
+        }
+    }
+    return true;
+}
+*/
+int main()
+{
+    string s1;
+    cout << "Enter the string " << endl;
+    getline(cin,s1);
+    if (valid_palindrom(s1))
+    {
+        cout << "String is a valid palindrom" << endl;
+    }
+    else
+    {
+        cout << "String is not a valid palindrom" << endl;
+    }
+    return 0;
+}
+/*
+bool valid_palindrom(string s1)
+{
+
+    string s;
+    for (int i=0;i<s1.length();i++)
+    {
+        if (isalnum(s1[i]))
+        {
+            s += tolower(s1[i]);
+        }
+    }
+    int l = 0;
+    int r = s.length() - 1;
+    while (l < r)
+    {
+        if (s[l] != s[r])
+        {
+            return false;
+        }
+        else
+        {
+            l++;
+            r--;
+        }
+    }
+    return true;
+}
+*/
