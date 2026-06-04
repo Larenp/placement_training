1c389fec2f924f78a5dda3017ca7a763787afd83
Laren <pintolaren05@gmail.com>
2026-06-04 09:41:31 +0530

    Refactor code structure for improved readability and maintainability

--- day6/unorderd_map.cpp
+++ day6/unorderd_map.cpp
@@ -1,15 +1,28 @@
-#include<iostream>
-#include<unordered_map>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
-int main(){
-    unordered_map<int,string>freq;
+int main()
+{
+    unordered_map<int, string> freq;
+    cout << "Enter the number of fruits : ";
+    int n = 0;
+    cin >> n;
+    string x;
+    /*
     freq[1]="apple";
-    freq[2]="banana";
-    freq[3]="orange";
-    freq[4]="grapes";
-
-    for(auto c:freq){
-        cout<<c.first<<" : "<<c.second<<endl;
+    freq[2]="banana"; 
+    */
+    for (auto i = 0; i < n; i++)
+    {
+        cout << "Enter the name of fruit : ";
+        cin >> x;
+        freq.insert({i, x});
     }
+    freq.erase(1);//removing the element with key 1
+    
 
+    for (auto c : freq)
+    {
+        cout << c.first << " : " << c.second << endl;
+    }
 }
