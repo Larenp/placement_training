50f7f9ad609f4551781cc96989528c3c4fcb1fd8
Laren <pintolaren05@gmail.com>
2026-06-05 12:22:55 +0530

    Refactor linked list implementation: improve formatting, add insert functions, and update traversal logic

--- day7/linked_list_basics.cpp
+++ day7/linked_list_basics.cpp
@@ -1,46 +1,69 @@
 #include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node(int val = 0) {
+    Node(int val = 0)
+    {
         data = val;
         next = nullptr;
     }
 };
 
-void traversal(Node* head) {
-    while (head != nullptr) {
+void traversal(Node *head)
+{
+    while (head != nullptr)
+    {
         cout << head->data << " -> ";
         head = head->next;
     }
     cout << "NULL" << endl;
 }
+void insert_at_end(Node *head,int val){
+    if(!head)return;
+    while(head->next){
+        head=head->next;
+    }
+    Node *newNode = new Node(val);
+    head->next= newNode;
 
-int main() {
-   
-    Node* head;
-    Node* n1 = new Node(5);
+}
+void insert_at_begging(Node *head, int val){
+    if(!head)return;
+  
+   Node *newNode =new Node(val);
+   newNode->next = head;
+   head = newNode;
+  
+}
+
+int main()
+{
+
+    Node *head;
+    Node *n1 = new Node(5);
     head = n1;
 
-    Node* n2 = new Node(15);
+    Node *n2 = new Node(15);
     n1->next = n2;
 
-    Node* n3 = new Node(20);
+    Node *n3 = new Node(20);
     n2->next = n3;
 
-    Node* n4 = new Node(25);
+    Node *n4 = new Node(25);
     n3->next = n4;
-
+    insert_at_end(head,30);
+    insert_at_begging(head,0);
     traversal(head);
 
     return 0;
 }
-//traversal
-//5 -> 15 -> 20 -> 25 -> NULL
+// traversal
+// 5 -> 15 -> 20 -> 25 -> NULL
 /*
 head
   |
@@ -49,5 +72,3 @@ head
 | 5 | *----->| 15 | *----->| 20 | *----->| 25 | NULL |
 +---+---+    +----+---+    +----+---+    +----+------+
 */
-
-
