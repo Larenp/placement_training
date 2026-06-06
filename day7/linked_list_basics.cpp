b3df3fc26c586288495ef01f582ed4a9ae95df45
Laren <pintolaren05@gmail.com>
2026-06-06 10:55:22 +0530

    Refactor linked list implementation: improve function formatting, add insert_at_middle and add_at_end functions, and update traversal logic

--- day7/linked_list_basics.cpp
+++ day7/linked_list_basics.cpp
@@ -23,56 +23,76 @@ void traversal(Node *head)
     }
     cout << "NULL" << endl;
 }
-void insert_at_end(Node *head,int val){
-    if(!head)return;
-    while(head->next){
-        head=head->next;
+void insert_at_end(Node *head, int val)
+{
+    if (!head)
+        return;
+    while (head->next)
+    {
+        head = head->next;
     }
     Node *newNode = new Node(val);
-    head->next= newNode;
+    head->next = newNode;
+}
+void insert_at_begging(Node *head, int val)
+{
+    if (!head)
+        return;
 
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
 }
-void insert_at_begging(Node *head, int val){
-    if(!head)return;
-  
-   Node *newNode =new Node(val);
-   newNode->next = head;
-   head = newNode;
-  
-}
-void delete_at_start(Node *head){
-    if(!head)return;
+void delete_at_start(Node *head)
+{
+    if (!head)
+        return;
     Node *temp = head;
     head = head->next;
     delete temp;
-
 }
-void delete_at_end(Node *head){
-    if(!head)return;
-    if(!head->next){
+void delete_at_end(Node *head)
+{
+    if (!head)
+        return;
+    if (!head->next)
+    {
         delete head;
         head = nullptr;
         return;
     }
     Node *temp = head;
-    while(temp->next->next){
+    while (temp->next->next)
+    {
         temp = temp->next;
     }
     delete temp->next;
     temp->next = nullptr;
-
 }
-void add_start(Node *head,int val){
-    Node *newNode =new Node(val);
-   newNode->next = head;
-   head = newNode;
+void add_start(Node *head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
 }
 
-void add_middle(Node *head,int val){
+void add_middle(Node *head, int val)
+{
     
-
-
+    Node *newNode = new Node(val);
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != nullptr)
+    {  fast = fast->next->next;
+        slow = slow->next;
+       
+    }
+    Node *nextnode = slow->next;
+    slow->next = newNode;
+    newNode->next = nextnode;
+    traversal(head);
 }
+
 
 int main()
 {
@@ -89,13 +109,16 @@ int main()
 
     Node *n4 = new Node(25);
     n3->next = n4;
-    insert_at_end(head,30);
-    insert_at_begging(head,0);
-    traversal(head);
-    delete_at_start(head);
-    traversal(head);
-    delete_at_end(head);
-    traversal(head);        
+    /*  insert_at_end(head,30);
+      insert_at_begging(head,0);
+      traversal(head);
+      delete_at_start(head);
+      traversal(head);
+      delete_at_end(head);
+      traversal(head);
+  */
+
+    add_middle(head, 100);
 
     return 0;
 }
--- day8/double_linked_list.cpp
+++ day8/double_linked_list.cpp
@@ -15,56 +15,96 @@ public:
         next = nullptr;
         prev = nullptr;
     }
-    
 };
 void traverse(Node *head)
 {
-   while(head){
-    cout<<head->data<<" -> ";
-    head=head->next;
-   }
-   cout<<"NULL"<<endl;
+    while (head)
+    {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
 }
 
-void add_start(Node *head, int val){
+void add_start(Node *head, int val)
+{
     Node *n = new Node(val);
-    if(head==NULL){
-        head=n;
-        head->next=NULL;
+    if (head == NULL)
+    {
+        head = n;
+        head->next = NULL;
     }
-    head->prev=n;
-    n->next=head;
-    head=n;
-traverse(head);
+    head->prev = n;
+    n->next = head;
+    head = n;
+}
+
+void insert_at_middle(Node *head, int val)
+{
+    Node *n = new Node(val);
+    Node *fast = head;
+    Node *slow = head;
+
+    while (fast != NULL && fast->next != nullptr)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    Node *prevNode = slow->prev;
+    n->prev = prevNode;
+    prevNode->next = n;
+
+    slow->prev = n;
+    n->next = slow;
+    traverse(head);
+}
+void add_at_end(Node *head, int val)
+{
+    Node *newNode = new Node(val);
+    if (!head)
+        head = newNode;
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+    newNode->next = NULL;
+
+    traverse(head);
 }
 
 int main()
 {
-Node *head;
+    Node *head;
 
-Node *n1 = new Node(5);
-head = n1;
+    Node *n1 = new Node(5);
+    head = n1;
 
-Node *n2 = new Node(10);
-n1->next = n2;
-n2->prev = n1;
+    Node *n2 = new Node(10);
+    n1->next = n2;
+    n2->prev = n1;
 
-Node *n3 = new Node(15);
-n2->next = n3;
-n3->prev = n2;
+    Node *n3 = new Node(15);
+    n2->next = n3;
+    n3->prev = n2;
 
-Node *n4 = new Node(20);
-n3->next = n4;
-n4->prev = n3;
+    Node *n4 = new Node(20);
+    n3->next = n4;
+    n4->prev = n3;
 
-Node *n5 = new Node(25);
-n4->next = n5;
-n5->prev = n4;
+    Node *n5 = new Node(25);
+    n4->next = n5;
+    n5->prev = n4;
+    Node *n6 = new Node(30);
+    n5->next = n6;
+    n6->prev = n5;
 
-
-
-add_start(head, 1);
-
+    add_start(head, 1);
+    insert_at_middle(head, 0);
+    add_at_end(head, 1);
 
     return 0;
 }
