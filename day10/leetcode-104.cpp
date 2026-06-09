f904e6998a5d6d8ae493d83957811cdd92c09c49
Laren <pintolaren05@gmail.com>
2026-06-09 11:45:57 +0530

    Add implementation of maxDepth function to calculate the maximum depth of a binary tree

---
+++ day10/leetcode-104.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root ==nullptr ){
+            return 0;
+        }
+    return 1+ max(maxDepth(root->left),maxDepth(root->right));
+        
+    }
+};
+/*
+maxDepth(3)
+├── maxDepth(9)
+│   ├── maxDepth(NULL) = 0
+│   └── maxDepth(NULL) = 0
+│   => 1
+└── maxDepth(20)
+    ├── maxDepth(15)
+    │   ├── 0
+    │   └── 0
+    │   => 1
+    └── maxDepth(7)
+        ├── 0
+        └── 0
+        => 1
+    => 2
+
+=> 1 + max(1,2) = 3
+*/
