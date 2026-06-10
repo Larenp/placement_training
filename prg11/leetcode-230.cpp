77964cb51b986a2df0c2d98ea1ec162f0eb2abf3
Laren <pintolaren05@gmail.com>
2026-06-10 11:48:46 +0530

    Add implementations for inorder traversal and kth smallest element in binary trees

---
+++ prg11/leetcode-230.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    void inorder(TreeNode* root) {
+        if (root == nullptr) {
+            return ;
+        }
+        inorder(root->left);
+        res.push_back(root->val);
+        inorder(root->right);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root);
+        sort(res.begin(), res.end());
+        int x = res[k - 1];
+        return x;
+    }
+};
