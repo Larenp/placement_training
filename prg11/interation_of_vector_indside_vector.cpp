6770aae7fb46fef57162f21cbe1e2acdcef0dea7
Laren <pintolaren05@gmail.com>
2026-06-10 12:39:40 +0530

    Add implementation for iterating through a vector of vectors and printing elements

---
+++ prg11/interation_of_vector_indside_vector.cpp
@@ -0,0 +1,12 @@
+for (int i = 0; i < res.size(); i++) {
+    for (int j = 0; j < res[i].size(); j++) {
+        cout << res[i][j] << " ";
+    }
+    cout << endl;
+}
+[[1],[2,3],[4,5,6]]
+res
+│
+├── {1}
+├── {2, 3}
+└── {4, 5, 6}
