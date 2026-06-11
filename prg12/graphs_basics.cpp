e994d6b991eb9588be64dff6c65c1b00c5eba372
Laren <pintolaren05@gmail.com>
2026-06-11 09:48:34 +0530

    Add initial documentation for graph types, representation, and traversal methods

---
+++ prg12/graphs_basics.cpp
@@ -0,0 +1,32 @@
+/*
+types of graphs
+1 directed
+2 undirected
+3 weighted
+4 cyclic/acyclic
+
+representation
+1 adj matrix
+2 adj list
+
+traversal
+1 bfs
+2 dfs
+
+eg:
+
+  0 1 2 3 
+0 0 0 0 0
+1     1 0 
+2     0
+3
+
+adj list
+
+0->1 3
+1->0 2
+2->1 3
+3->0 2
+
+
+*/
