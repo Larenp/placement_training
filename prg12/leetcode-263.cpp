54ea290c4f73395ef6ceff42e586929b87fcc5f0
Laren <pintolaren05@gmail.com>
2026-06-11 15:11:08 +0530

    Add implementation for calculating island perimeter in a grid

---
+++ prg12/leetcode-263.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
+        int row = grid.size();
+        int col = grid[0].size();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+                    if (i > 0 && grid[i - 1][j] == 1)
+                        perimeter -= 2;
+
+                    if (j > 0 && grid[i][j - 1] == 1)
+                        perimeter -= 2;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
+/*class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int perimeter = 0;
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+
+                if(grid[i][j] == 1) {
+
+                    // Top
+                    if(i == 0 || grid[i-1][j] == 0)
+                        perimeter++;
+
+                    // Bottom
+                    if(i == rows-1 || grid[i+1][j] == 0)
+                        perimeter++;
+
+                    // Left
+                    if(j == 0 || grid[i][j-1] == 0)
+                        perimeter++;
+
+                    // Right
+                    if(j == cols-1 || grid[i][j+1] == 0)
+                        perimeter++;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};*/
